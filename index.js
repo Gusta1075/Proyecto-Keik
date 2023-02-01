8281e4d1c5e5d92a7282114dafe440dad3e6eedf
Gusta1075 <gustavosanmartino.gs@gmail.com>
2023-02-01 19:55:08 +0100

    Actualizacion

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //practicas de alert
 let nombre = prompt("ingrese su nombre");
-alert ("Bienvenido a mi pagina") `${nombre}`;
+alert("Bienvenido a mi pagina " + nombre);
 let edad = prompt("ingrese edad");
 alert("Muchas gracias por brindarme su edad " + nombre);
 
