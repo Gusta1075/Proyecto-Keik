608839daf62ab8920e96ac4157aeba71fcdbd97f
Gusta1075 <gustavosanmartino.gs@gmail.com>
2023-02-01 19:16:58 +0100

    Nuevas alctualizaciones

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+//practicas de alert
 let nombre = prompt("ingrese su nombre");
-alert("Bienvenido a mi pagina " + nombre);
+alert ("Bienvenido a mi pagina") `${nombre}`;
 let edad = prompt("ingrese edad");
 alert("Muchas gracias por brindarme su edad " + nombre);
 
+// practicas de condicionales
