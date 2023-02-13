2e4955253b626618cc073483c1f27129f8205d0a
Gusta1075 <gustavosanmartino.gs@gmail.com>
2023-02-13 20:15:26 +0100

    arreglo

--- Assets/Prendas-mujer/main.js
+++ Assets/Prendas-mujer/main.js
@@ -1,21 +1,25 @@
 let container = document.getElementById("container");
 
 const productos= [
-{ id: 1, nombre: "Camisa blanca", precio: 1250 },
-{ id: 2, nombre: "Camisa verde ", precio: 1150 },
-{ id: 3, nombre: "Camisa Amarilla", precio: 1350 },
-{ id: 4, nombre: "Camisa Gris", precio: 1450 },
-{ id: 5, nombre: "Camisa Azul", precio: 750 },
+{ id: 1, nombre: "Camisa blanca", precio: 1250, seccion: "verano" },
+{ id: 2, nombre: "Camisa verde ", precio: 1150, seccion: "invierno" },
+{ id: 3, nombre: "Camisa Amarilla", precio: 1350, seccion: "primavera"},
+{ id: 4, nombre: "Camisa Gris", precio: 1450, seccion: "otoño" },
+{ id: 5, nombre: "Camisa Azul", precio: 750, seccion: "indefinido"},
 ];
 
 
 productos.forEach(item => {
 let div = document.createElement("div");
 div.innerHTML = `
-<h2>Id: ${item.id}</h2>
+<h1>Id: ${item.id}</h1>
+<h2>Seccion: ${item.seccion}</h2>
 <p>Nombre:${item.nombre}<p>
 <b>Precio: ${item.precio}</b>`;
 
 container.append(div);
 
 })
+
+
+
